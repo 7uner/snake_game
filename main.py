fd2f20a12f860ca2041d6e0e82279cb9f191c730
cath-yxw <yxw.cath@gmail.com>
2024-08-30 14:02:54 -0400

    2 player

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 # Game dimensions
 WIDTH = 800
@@ -16,7 +17,7 @@ HEIGHT = 600
 # Snake settings
 SNAKE_SIZE = 20
 SPEED = 10
-colors = (WHITE, BLACK, RED, GREEN)
+colors = (WHITE, BLACK, RED, GREEN, BLUE)
 
 if __name__ == "__main__":
     game = SnakeGame(colors, WIDTH, HEIGHT, SNAKE_SIZE, SPEED)
--- snakegame.py
+++ snakegame.py
@@ -2,27 +2,89 @@ import pygame
 import random
 import time
 import sys
-import snake
+
+class Snake:
+    def __init__(self, win, color, snake_size=20, speed=15, start_pos=[[100, 60], [80, 60], [60, 60]], direction='RIGHT'):
+        self.win = win
+        self.color = color
+        self.SNAKE_SIZE = snake_size
+        self.speed = speed
+        self.snake_pos = start_pos
+        self.snake_direction = direction
+        self.change_to = self.snake_direction
+
+    def direction_event_handler(self, events, player=1):
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if player == 1:
+                    if event.key == pygame.K_UP:
+                        if self.change_to != 'DOWN':
+                            self.change_to = 'UP'
+                    elif event.key == pygame.K_DOWN:
+                        if self.change_to != 'UP':
+                            self.change_to = 'DOWN'
+                    elif event.key == pygame.K_LEFT:
+                        if self.change_to != 'RIGHT':
+                            self.change_to = 'LEFT'
+                    elif event.key == pygame.K_RIGHT:
+                        if self.change_to != 'LEFT':
+                            self.change_to = 'RIGHT'
+                elif player == 2:
+                    if event.key == pygame.K_w:
+                        if self.change_to != 'DOWN':
+                            self.change_to = 'UP'
+                    elif event.key == pygame.K_s:
+                        if self.change_to != 'UP':
+                            self.change_to = 'DOWN'
+                    elif event.key == pygame.K_a:
+                        if self.change_to != 'RIGHT':
+                            self.change_to = 'LEFT'
+                    elif event.key == pygame.K_d:
+                        if self.change_to != 'LEFT':
+                            self.change_to = 'RIGHT'
+
+    def move_snake(self):
+        if self.change_to == 'UP':
+            self.snake_direction = 'UP'
+        if self.change_to == 'DOWN':
+            self.snake_direction = 'DOWN'
+        if self.change_to == 'LEFT':
+            self.snake_direction = 'LEFT'
+        if self.change_to == 'RIGHT':
+            self.snake_direction = 'RIGHT'
+
+        new_head = list(self.snake_pos[0])
+        if self.snake_direction == 'UP':
+            new_head[1] -= self.SNAKE_SIZE
+        if self.snake_direction == 'DOWN':
+            new_head[1] += self.SNAKE_SIZE
+        if self.snake_direction == 'LEFT':
+            new_head[0] -= self.SNAKE_SIZE
+        if self.snake_direction == 'RIGHT':
+            new_head[0] += self.SNAKE_SIZE
+
+        self.snake_pos.insert(0, new_head)
+
+    def draw_snake(self):
+        for pos in self.snake_pos:
+            pygame.draw.rect(self.win, self.color, pygame.Rect(pos[0], pos[1], self.SNAKE_SIZE, self.SNAKE_SIZE))
+
 
 class SnakeGame:
     def __init__(self, colors, width, height, snake_size=20, speed=15):
         self.WIDTH = width
         self.HEIGHT = height
-        self.score = 0
+        self.score1 = 0
+        self.score2 = 0
 
-        self.WHITE, self.BLACK, self.RED, self.GREEN = colors
+        self.WHITE, self.BLACK, self.RED, self.GREEN, self.BLUE = colors
 
         pygame.init()
         self.win = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
-        pygame.display.set_caption("Snake Game")
+        pygame.display.set_caption("Multiplayer Snake Game")
         self.clock = pygame.time.Clock()
 
-        # # Snake settings
-        # self.snake_pos = [[100, 60], [80, 60], [60, 60]]
-        # self.snake_direction = 'RIGHT'
-        # self.change_to = self.snake_direction
-
-        self.food_pos = self.random_food_position(20)
+        self.food_pos = self.random_food_position(snake_size)
         self.food_spawn = True
 
     def random_food_position(self, SNAKE_SIZE):
@@ -31,7 +93,7 @@ class SnakeGame:
 
     def game_over(self):
         font = pygame.font.SysFont('times new roman', 50)
-        GO_surf = font.render('Your Score is : ' + str(self.score), True, self.RED)
+        GO_surf = font.render(f'Player 1 Score: {self.score1}, Player 2 Score: {self.score2}', True, self.RED)
         GO_rect = GO_surf.get_rect()
         GO_rect.midtop = (self.WIDTH / 2, self.HEIGHT / 4)
         self.win.blit(GO_surf, GO_rect)
@@ -43,37 +105,40 @@ class SnakeGame:
     def draw_food(self, SNAKE_SIZE):
         pygame.draw.rect(self.win, self.RED, pygame.Rect(self.food_pos[0], self.food_pos[1], SNAKE_SIZE, SNAKE_SIZE))
 
-
-    # draw a grid to help us validate movement, can keep or remove
-    def draw_grid(self, SNAKE_SIZE = 20):
+    def draw_grid(self, SNAKE_SIZE=20):
         for i in range(self.WIDTH // SNAKE_SIZE):
-            pygame.draw.line(self.win, self.WHITE, (i * SNAKE_SIZE, 0),
-                             (i * SNAKE_SIZE, self.HEIGHT))
+            pygame.draw.line(self.win, self.WHITE, (i * SNAKE_SIZE, 0), (i * SNAKE_SIZE, self.HEIGHT))
         for i in range(self.HEIGHT // SNAKE_SIZE):
-            pygame.draw.line(self.win, self.WHITE, (0, i * SNAKE_SIZE),
-                             (self.WIDTH, i * SNAKE_SIZE))
+            pygame.draw.line(self.win, self.WHITE, (0, i * SNAKE_SIZE), (self.WIDTH, i * SNAKE_SIZE))
 
     def run(self):
-        # initialize the snake object
-        snake1 = snake.Snake(self.win, self.GREEN)
+        snake1 = Snake(self.win, self.GREEN)
+        snake2 = Snake(self.win, self.BLUE, start_pos=[[300, 300], [280, 300], [260, 300]], direction='LEFT')
+
         while True:
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            # event button handling for the snake
-            snake1.direction_event_handler(events)
 
-            # move snake based on where the head is
+            snake1.direction_event_handler(events, player=1)
+            snake2.direction_event_handler(events, player=2)
+
             snake1.move_snake()
+            snake2.move_snake()
 
-            # grow snake if the snake consumed food
             if snake1.snake_pos[0] == self.food_pos:
-                self.score += 1
+                self.score1 += 1
                 self.food_spawn = False
             else:
                 snake1.snake_pos.pop()
+
+            if snake2.snake_pos[0] == self.food_pos:
+                self.score2 += 1
+                self.food_spawn = False
+            else:
+                snake2.snake_pos.pop()
 
             if not self.food_spawn:
                 self.food_pos = self.random_food_position(20)
@@ -83,18 +148,22 @@ class SnakeGame:
 
             self.draw_grid()
             snake1.draw_snake()
+            snake2.draw_snake()
             self.draw_food(20)
 
-            # game over conditions per snake can be moved to the snake class
             if (snake1.snake_pos[0][0] < 0 or snake1.snake_pos[0][0] >= self.WIDTH or
-                snake1.snake_pos[0][1] < 0 or snake1.snake_pos[0][1] >= self.HEIGHT):
+                snake1.snake_pos[0][1] < 0 or snake1.snake_pos[0][1] >= self.HEIGHT or
+                snake2.snake_pos[0][0] < 0 or snake2.snake_pos[0][0] >= self.WIDTH or
+                snake2.snake_pos[0][1] < 0 or snake2.snake_pos[0][1] >= self.HEIGHT):
                 self.game_over()
 
-            # Game Over conditions
-            if snake1.snake_pos[0][0] < 0 or snake1.snake_pos[0][0] > (self.WIDTH - snake1.SNAKE_SIZE):
-                self.game_over()
-            if snake1.snake_pos[0][1] < 0 or snake1.snake_pos[0][1] > (self.HEIGHT - snake1.SNAKE_SIZE):
-                self.game_over()
+            for block in snake1.snake_pos[1:]:
+                if snake1.snake_pos[0] == block or snake2.snake_pos[0] == block:
+                    self.game_over()
+
+            for block in snake2.snake_pos[1:]:
+                if snake1.snake_pos[0] == block or snake2.snake_pos[0] == block:
+                    self.game_over()
 
             pygame.display.update()
             self.clock.tick(snake1.speed)
